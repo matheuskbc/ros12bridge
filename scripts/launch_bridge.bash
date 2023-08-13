ddf706f8d2cea0ee45496aa741bc519003208f0c
Matheus <matheuskramer11@gmail.com>
2023-08-13 18:14:43 +0200

    add support to tmux

--- scripts/launch_bridge.bash
+++ scripts/launch_bridge.bash
@@ -1,7 +1,11 @@
+#! /bin/bash
+tmux new-session -d
 
 # Setup environment variables 
 # Load ros1 and ros2
-source ./scripts/setup_bridge.bash
+tmux send-keys -t 0 'source ./scripts/setup_bridge.bash' C-m
 
 # Run ros bridge
-bash ./scripts/run_ros_bridge_1_to_2_full.bash
+tmux send-keys -t 0 'bash ./scripts/run_ros_bridge_1_to_2_full.bash' C-m
+
+tmux -2 attach-session 
