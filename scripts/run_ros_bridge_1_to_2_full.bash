563ee3e6824c1548924513f9e2f30dc4c3f9ab56
Matheus <matheuskramer11@gmail.com>
2024-12-26 16:38:36 +0100

    fix: redo previous

--- scripts/run_ros_bridge_1_to_2_full.bash
+++ scripts/run_ros_bridge_1_to_2_full.bash
@@ -1,6 +1,6 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source $SCRIPT_DIR/setup_bridge.bash
-# export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export ROS_MASTER_URI=http://localhost:11311
 export ROS_HOSTNAME=ubuntu
 ros2 run ros1_bridge dynamic_bridge --bridge-all-1to2-topics
